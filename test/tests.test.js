d13446cc773f13e1cd7a6199706856e9991acdcd
Will Johnson <william.c.johnson@intel.com>
2017-04-26 15:40:13 -0400

    - CR updates

--- test/tests.test.js
+++ test/tests.test.js
@@ -1,13 +1,12 @@
 // @flow
 
 import extractApi from '../index.js';
-import { format } from 'util';
 
 import { describe, it, expect } from './jasmine.js';
 
 describe('Extract API', function() {
   ['/api/host/10', '/api/host/10/', 'api/host/10'].forEach(function(path) {
-    it(format('should extract the path id from %s', path), function() {
+    it(`should extract the path id from ${path}`, function() {
       expect(extractApi(path)).toEqual('10');
     });
   });
