c3829a33f1b21e8f7b396a775721f084e9906b58
Joe Grund <joe.grund@intel.com>
2017-05-10 12:01:54 -0400

    Update function to throw. (#4)
    Update the call to throw when an id is not found.
    This will enable us to not have to null check the result
    of this call.
    
    If we need to, we can also add a tryExtractApi that returns a maybe
    later on.
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- test/tests.test.js
+++ test/tests.test.js
@@ -5,9 +5,15 @@ import extractApi from '../index.js';
 import { describe, it, expect } from './jasmine.js';
 
 describe('Extract API', function() {
-  ['/api/host/10', '/api/host/10/', 'api/host/10'].forEach(function(path) {
-    it(`should extract the path id from ${path}`, function() {
+  ['/api/host/10', '/api/host/10/', 'api/host/10'].forEach(path => {
+    it(`should extract the path id from ${path}`, () => {
       expect(extractApi(path)).toEqual('10');
     });
+  });
+
+  it('should throw when string is not found', () => {
+    expect(() => extractApi('foo')).toThrow(
+      'could not extract api id from foo'
+    );
   });
 });
